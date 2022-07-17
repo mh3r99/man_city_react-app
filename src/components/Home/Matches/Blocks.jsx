7bc84fb099734383b14e50cb71e7443d788a6be8
Mher Hovhannisyan <mh3r99@gmail.com>
2022-07-17 18:38:54 +0400

    home matches final

--- src/components/Home/Matches/Blocks.jsx
+++ src/components/Home/Matches/Blocks.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Slide } from "react-awesome-reveal";
 import { matchesCollection } from "../../../firebase";
 import { getDocs } from "firebase/firestore";
+import MatchesBlock from "../../ui/MatchesBlock";
 
 const Blocks = () => {
   const [matches, setMatches] = useState([]);
@@ -23,7 +24,20 @@ const Blocks = () => {
     }
   }, [matches]);
 
-  return <div>hello</div>;
+  const showMatches = (matches) =>
+    matches
+      ? matches.map((match) => (
+          <Slide bottom key={match.id} className="item" triggerOnce>
+            <div>
+              <div className="wrapper">
+                <MatchesBlock match={match} />
+              </div>
+            </div>
+          </Slide>
+        ))
+      : null;
+
+  return <div className="home_matches">{showMatches(matches)}</div>;
 };
 
 export default Blocks;
