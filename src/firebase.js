0a6588d01e2dc3cf25dfa58bce9ee920b38090be
Mher Hovhannisyan <mh3r99@gmail.com>
2022-07-17 14:30:21 +0400

    import data to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection, doc, setDoc } from "firebase/firestore";
+
+import { cityDb } from "./temp/m-city-export";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD8ACrDynoUcoGCOdBVM8FwKBZkp6xGnwY",
@@ -13,3 +15,30 @@ const firebaseConfig = {
 // Initialize Firebase
 initializeApp(firebaseConfig);
 export const db = getFirestore();
+
+export const matchesCollection = collection(db, "matches");
+export const playersCollection = collection(db, "players");
+export const positionsCollection = collection(db, "positions");
+export const promotionsCollection = collection(db, "promotions");
+export const teamsCollection = collection(db, "teams");
+
+// import data to firebase
+// cityDb.matches.forEach((item) => {
+//   setDoc(doc(matchesCollection), item);
+// });
+
+// cityDb.players.forEach((item) => {
+//   setDoc(doc(playersCollection), item);
+// });
+
+// cityDb.positions.forEach((item) => {
+//   setDoc(doc(positionsCollection), item);
+// });
+
+// cityDb.promotions.forEach((item) => {
+//   setDoc(doc(promotionsCollection), item);
+// });
+
+// cityDb.teams.forEach((item) => {
+//   setDoc(doc(teamsCollection), item);
+// });
