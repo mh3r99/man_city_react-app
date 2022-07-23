f9ad1b236d53f7f5ee33c2730cfb08a94dfc92a0
Mher Hovhannisyan <mh3r99@gmail.com>
2022-07-23 23:42:35 +0400

    not found

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import TheTeam from "./components/TheTeam/TheTeam";
 import Matches from "./components/Admin/Matches/Matches";
 import AddEditMatches from "./components/Admin/Matches/AddEditMatches";
 import TheMatches from "./components/TheMatches/TheMatches";
+import NotFound from "./NotFound/NotFound";
 
 const App = () => {
   return (
@@ -50,6 +51,7 @@ const App = () => {
               element={<AddEditMatches />}
             />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Switch>
         <ToastContainer />
         <Footer />
